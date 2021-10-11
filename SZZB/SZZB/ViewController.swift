cab823e24518b48d245e080716b68e361f69c66b
zhujian <130045275@qq.com>
2021-10-11 12:36:41 +0800

    这是一个初始化

---
+++ SZZB/SZZB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SZZB
+//
+//  Created by 朱建 on 2021/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
